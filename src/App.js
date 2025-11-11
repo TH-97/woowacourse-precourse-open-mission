485952fcce7a8b51abaf82d3f6209d7f455089bd
TH_97 <hood3392@gmail.com>
2025-11-11 19:02:47 +0900

    feat(controller) : class, spec 정보 map 가져오기 기능 추가

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
+import { classController } from "./controller/classController/classController.js";
 import { seasonController } from "./controller/seasonController/seasonController.js";
 
 class App {
   async run() {
     const encounterMap = seasonController.loadCurrentSeasonEncounterMap();
+    const classNameToIdMap = classController.loadClassNameToIdMap();
+    const classSpecNameToIdMap = classController.loadClassSpecNameToIdMap();
   }
 }
 
---
+++ src/controller/classController/handler/loadClassesAndSpecs.js
@@ -0,0 +1,5 @@
+import __mock__classesAndSpecsData from "../../../../__mock__/classes_specs_data.mock.json" with {type : "json"}
+
+export function loadClassesAndSpecs() {
+  return __mock__classesAndSpecsData
+}
