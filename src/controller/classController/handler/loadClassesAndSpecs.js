7374328dbe591306371eb84fb0ba0fb11383532c
TH_97 <hood3392@gmail.com>
2025-11-20 03:34:19 +0900

    efactor(controller) : 기능 전면 수정으로 인한 필요없는 파일 삭제

--- src/controller/classController/handler/loadClassesAndSpecs.js
+++
@@ -1,5 +0,0 @@
-import __mock__classesAndSpecsData from "../../../../__mock__/classes_specs_data.mock.json" with {type : "json"}
-
-export function loadClassesAndSpecs() {
-  return __mock__classesAndSpecsData
-}
--- src/controller/dataController/handler/loadData.js
+++
@@ -1,9 +0,0 @@
-export function loadData(classId, specId) {
-  const data = import(
-    `../__mock__/class${classId}_spec${specId}_page1.mock.json`,
-    {
-      with: { type: "json" },
-    }
-  );
-  return data;
-}
