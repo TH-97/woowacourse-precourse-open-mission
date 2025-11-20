e900e95e63c7661721f84e88fdc89bf665072504
TH_97 <hood3392@gmail.com>
2025-11-20 19:30:34 +0900

    feat(constants) : 상수 추가

---
+++ src/constants/index.js
@@ -0,0 +1,8 @@
+export const API = {
+  WARCRAFTLOG_URL: "https://api.warcraftlogs.com/v1/seasons",
+};
+
+export const ERROR = {
+  EMPTY: "[ERROR] 값이 비어있습니다",
+  IS_NOT_CURRENT_SEASON: "[ERROR] 현재 시즌이 아닙니다",
+};
