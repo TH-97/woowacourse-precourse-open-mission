7218d8587d43b376156e27fa7c7756e5d72e603d
TH_97 <hood3392@gmail.com>
2025-11-21 19:25:38 +0900

    feat(constanats) : 상수 추가

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,8 +1,12 @@
 export const API = {
-  WARCRAFTLOG_URL: "https://api.warcraftlogs.com/v1/seasons",
+  WARCRAFTLOG_ZONE_URL: "https://www.warcraftlogs.com:443/v1/zones?",
+  WARCRAFTLOG_CLASS_URL: "https://www.warcraftlogs.com:443/v1/classes?",
 };
 
 export const ERROR = {
   EMPTY: "[ERROR] 값이 비어있습니다",
   IS_NOT_CURRENT_SEASON: "[ERROR] 현재 시즌이 아닙니다",
+  API_KEY_NOT_SET: "[ERROR] API Key가 설정되지 않았습니다",
+  API_REQUEST_FAILED: "[ERROR] API 요청 실패",
+  API_REQUEST_ERROR: "[ERROR] API 요청 중 에러 발생",
 };
