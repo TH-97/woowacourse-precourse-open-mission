fe4b3369c58dfa935c78531d2b7ba0396ca955cb
TH_97 <hood3392@gmail.com>
2025-11-21 19:28:38 +0900

    feat(service) : 시즌 service 구현
     - api를 통한 data 불러오기
     - 실패했을때를 대비하여 mock 데이터 불러오기
     - 현재 시즌 찾기
     - 현재 시즌 db에 저장

---
+++ src/service/seasonService.js
@@ -0,0 +1,48 @@
+import { API } from "../constants/index.js";
+import __mock__apiData from "../../__mock__/season.mock.json" with {type : "json"};
+import { ERROR } from "../constants/index.js";
+import { CurrentSeasonDto } from "../dto/currentSeasonDto.js";
+import { EncounterDAO } from "../dao/EncounterDAO.js";
+import { Encounters } from "../model/Encounters.model.js";
+import { db } from "../db/db.js";
+import 'dotenv/config';
+
+const API_KEY = process.env.WARCRAFTLOG_API_KEY;
+const URL = API.WARCRAFTLOG_ZONE_URL;
+
+export const seasonService = {
+  async loadCurrentSeasonData() {
+    if (!API_KEY) throw new Error(ERROR.API_KEY_NOT_SET);
+
+    const url = `${URL}apikey=${API_KEY}`;
+
+    try {
+      const response = await fetch(url);
+      if (!response.ok) {
+        throw new Error(`${ERROR.API_REQUEST_FAILED}: ${response.status}`);
+      }
+      const data = await response.json();
+      return data;
+    } catch (e) {
+      throw new Error(ERROR.API_REQUEST_ERROR);
+    }
+  },
+
+  async loadMockCurrentSeason() {
+    const data = __mock__apiData;
+    return data;
+  },
+
+  async findCurrentSeasonData(data) {
+    const currentSeasonData = data.find(season => season.id === 45);
+    const encounters = new CurrentSeasonDto(currentSeasonData).getEncounters()
+
+    return encounters
+  },
+
+  async saveCurrentSeasonEncounters(encounters){
+    const dao = new EncounterDAO(db)
+    const dungeonInfo = new Encounters(encounters).getTransformEncounters()
+    dao.insert(dungeonInfo)
+  },
+};
