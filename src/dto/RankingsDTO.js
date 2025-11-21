cffb727f0c820253cd9f620e23d2089b36ccd54b
TH_97 <hood3392@gmail.com>
2025-11-22 06:13:06 +0900

    feat(DTO) : 랭커들의 데이터 DTO 구현

---
+++ src/dto/RankingsDTO.js
@@ -0,0 +1,17 @@
+import { ERROR } from "../constants/index.js";
+
+export class RankingsDTO {
+  #gear;
+
+  constructor(data) {
+    this.#validate(data);
+    this.#gear = data;
+  }
+  #validate(data) {
+    if (data.length === 0) throw new Error(ERROR.EMPTY);
+  }
+
+  getGear() {
+    return this.#gear;
+  }
+}
