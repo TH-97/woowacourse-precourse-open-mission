26c307db9319bafae03ddd2a785315161355141c
TH_97 <hood3392@gmail.com>
2025-11-22 06:14:47 +0900

    feat(service) : 랭커들 service 구현
     - 던전별, 직업별, 영웅특성별 데이터 불러오기
     - 직업별, 영웅특성별 mock데이터 불러오기
     - 랭커들의 item DTO 형태로 반환

---
+++ src/service/rankingsService.js
@@ -0,0 +1,38 @@
+import { API } from "../constants/index.js";
+import { db } from "../db/db.js";
+import "dotenv/config";
+import fs from "fs/promises";
+import { RankingsDTO } from "../dto/RankingsDTO.js";
+
+const API_KEY = process.env.WARCRAFTLOG_API_KEY;
+const URL = API.WARCRAFTLOG_RANKINGS_URL;
+
+export const rankingsService = {
+  async loadRankingsData(encounterId, classId, specId) {
+    if (!API_KEY) throw new Error(ERROR.API_KEY_NOT_SET);
+
+    const url = `${URL}${encounterId}?class=${classId}&spec=${specId}&includeCombatantInfo=true&api_key=${API_KEY}`;
+
+    try {
+      const response = await fetch(url);
+      if (!response.ok) {
+        throw new Error(`${ERROR.API_REQUEST_FAILED}: ${response.status}`);
+      }
+      const data = await response.json();
+      return data;
+    } catch (e) {
+      throw new Error(ERROR.API_REQUEST_ERROR);
+    }
+  },
+  async loadMockRankingsData(classId, specId) {
+    const data = await fs.readFile(
+      `../../__mock__/class${classId}_spec${specId}_page1.mock.json`
+    );
+    return data;
+  },
+  async getGear(data) {
+    const gearDTOs = data.map((ranking) => new RankingsDTO(ranking.gear));
+
+    return gearDTOs;
+  },
+};
