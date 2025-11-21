18c2621886632218136a043217e0da989676497e
TH_97 <hood3392@gmail.com>
2025-11-21 19:26:36 +0900

    refactor : 필요 없어진 파일 삭제

--- src/controller/seasonController/handler/loadCurrentSeason.js
+++
@@ -1,6 +0,0 @@
-import __mock__apiData from "../../../../__mock__/season.mock.json" with {type : "json"};
-
-export function loadCurrentSeason(targetSeason) {
-  const findCurrentSeason = __mock__apiData.find((season) => season.id === targetSeason);
-  return findCurrentSeason;
-}
