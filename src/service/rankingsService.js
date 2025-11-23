c5d32258ec404d92ac72803b361537796e2f8839
TH_97 <hood3392@gmail.com>
2025-11-24 03:34:16 +0900

    refactor(service) : 랭커 service 변수명 수정

--- src/service/rankingsService.js
+++ src/service/rankingsService.js
@@ -1,17 +1,17 @@
 import { API } from "../constants/index.js";
-import { db } from "../db/db.js";
 import "dotenv/config";
 import fs from "fs/promises";
+import { URL } from "node:url";
 import { GearListDTO } from "../dto/GearListDTO.js";
 
 const API_KEY = process.env.WARCRAFTLOG_API_KEY;
-const URL = API.WARCRAFTLOG_RANKINGS_URL;
+const RANKINGS_URL = API.WARCRAFTLOG_RANKINGS_URL;
 
 export const rankingsService = {
   async loadRankingsData(encounterId, classId, specId) {
     if (!API_KEY) throw new Error(ERROR.API_KEY_NOT_SET);
 
-    const url = `${URL}${encounterId}?class=${classId}&spec=${specId}&includeCombatantInfo=true&api_key=${API_KEY}`;
+    const url = `${RANKINGS_URL}${encounterId}?class=${classId}&spec=${specId}&includeCombatantInfo=true&api_key=${API_KEY}`;
 
     try {
       const response = await fetch(url);
@@ -25,13 +25,18 @@ export const rankingsService = {
     }
   },
   async loadMockRankingsData(classId, specId) {
-    const data = await fs.readFile(
-      `../../__mock__/class${classId}_spec${specId}_page1.mock.json`
+    const fileUrl = new URL(
+      `../../__mock__/class${classId}_spec${specId}_page1.mock.json`,
+      import.meta.url
     );
+
+    const data = await fs.readFile(fileUrl, "utf-8");
     return data;
   },
   async getGear(data) {
-    const gearListDTOs = data.map((ranking) => new GearListDTO(ranking));
+    const gearListDTOs = data.rankings.map(
+      (info) => new GearListDTO(info.gear)
+    );
 
     return gearListDTOs;
   },
