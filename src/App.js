28ba1df3e40ba4686e649e0a34244727cdd2a93a
TH_97 <hood3392@gmail.com>
2025-11-24 05:15:57 +0900

    fix(App) : npm start를 위한 수정

--- src/App.js
+++ src/App.js
@@ -17,6 +17,12 @@ class App {
     );
     gearController.syncRecommendItem(gearListDTOs);
   }
+  async run() {
+    await seasonController.syncSeasonData();
+    await classController.syncClassAndSpecData();
+    const gearListDTOs = await rankingsController.getGearDTOs(62660, 1, 1);
+    gearController.syncRecommendItem(gearListDTOs);
+  }
 }
 
 export default new App();
