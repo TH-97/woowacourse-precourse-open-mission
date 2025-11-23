34b3eb2b2708fe94352123f8d6da7e70fe612158
TH_97 <hood3392@gmail.com>
2025-11-24 07:27:41 +0900

    fix(service) : icon 주소 추가

--- src/service/gearService.js
+++ src/service/gearService.js
@@ -1,5 +1,5 @@
 import { GearDAO } from "../dao/GearDAO.js";
-import { Gear } from "../model/Gear.model.js";
+import { Gear } from "../model/gear.model.js";
 import { db } from "../db/db.js";
 
 export const gearService = {
@@ -9,14 +9,15 @@ export const gearService = {
 
     const recommendItems = this.calculateRecommendItems(gear);
 
-    dao.insert(recommendItems);
+    await dao.insert(recommendItems);
   },
+
   calculateRecommendItems(gear) {
-    const recommendItems = {
+    return {
       head: this.findRecommendItem(gear.getHeads()),
       back: this.findRecommendItem(gear.getBacks()),
       chest: this.findRecommendItem(gear.getChests()),
-      feet: this.findRecommendItem(gear.getFeets()),
+      feet: this.findRecommendItem(gear.getFeet()),
       fingers: this.findRecommendSpecialSlot(gear.getFingers()),
       hands: this.findRecommendItem(gear.getHands()),
       legs: this.findRecommendItem(gear.getLegs()),
@@ -27,48 +28,38 @@ export const gearService = {
       waist: this.findRecommendItem(gear.getWaists()),
       wrist: this.findRecommendItem(gear.getWrists()),
     };
-
-    return recommendItems;
   },
 
-  findRecommendItem(items) {
+  buildCountMap(items) {
     const countMap = new Map();
-
     for (const item of items) {
-      countMap.set(item, (countMap.get(item) || 0) + 1);
+      const key = `${item.name}:${item.icon}`;
+      countMap.set(key, (countMap.get(key) || 0) + 1);
     }
-
-    const [item] = this.sortItem(countMap)[0];
-    return item;
-  },
-
-  sortItem(countMap) {
     return Array.from(countMap.entries()).sort((a, b) => b[1] - a[1]);
   },
 
+  findRecommendItem(items) {
+    const sorted = this.buildCountMap(items);
+
+    const [key] = sorted[0];
+    const [name, icon] = key.split(":");
+    return { name, icon };
+  },
+
   findRecommendSpecialSlot(items) {
-    const countMap = new Map();
+    const sorted = this.buildCountMap(items);
 
-    for (const item of items) {
-      countMap.set(item, (countMap.get(item) || 0) + 1);
-    }
+    console.log(sorted);
+    const [key1] = sorted[0];
+    const [key2] = sorted[1] || [null];
 
-    const sorted = this.sortItem(countMap);
+    const [name1, icon1] = key1.split(":");
+    const item1 = { name: name1, icon: icon1 };
 
-    let [item1] = sorted[0];
-    let [item2] = sorted[1];
-
-    if (this.isSameItem(item1, item2)) {
-      [item2] = sorted[2] ?? [null];
-    }
+    const [name2, icon2] = key2.split(":");
+    let item2 = { name: name2, icon: icon2 };
 
     return [item1, item2];
-  },
-  isSameItem(item1, item2) {
-    if (!item1 || !item2) return false;
-
-    const itam1Name = item1.name.split(" ")[0];
-    const itam2Name = item2.name.split(" ")[0];
-    return itam1Name === itam2Name;
   },
 };
